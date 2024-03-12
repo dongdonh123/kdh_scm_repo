3764bce335a907ee039a96ea82adfe48ac35ceec
김동환 <kdhqnwk@naver.com>
2024-03-12 21:59:18 +0900

    1일차 종료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import backgroundImage from './image/background.jpg'; // 이미지 파일의 경로
 
 function App() {
-  return <div className="App"></div>;
+  // 배경화면 이미지 경로
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100vw',
+        height: '100vh',
+      }}
+    ></div>
+  );
 }
 
 export default App;
